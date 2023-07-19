20167e85509478ac7e41fde05a0c8e66cc2c8bf7
Rubeena-zt <rubeena.pk@zaeemsolutions.com>
2023-07-19 17:41:22 +0530

    created and api integrated the total alarms component

--- App.tsx
+++ App.tsx
@@ -1,40 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  // SafeAreaView,
+  // ScrollView,
+  // StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
-import DriverProfile from './components/widgets/DriverProfile';
+import TotalAlarms from './components/widgets/TotalAlarms';
 
 function App(): JSX.Element {
   return (
-   <View>
-    <Text>ygfurtgr</Text>
-    <DriverProfile/>
-   </View>
+    <View style={styles.mainContainer}>
+      <View style={styles.firstPart}>
+        <Text>1st part</Text>
+      </View>
+      <View style={styles.secondPart}>
+        <Text>2nd part</Text>
+        <TotalAlarms />
+      </View>
+      <View style={styles.thirdPart}>
+        <Text>3rd part</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  mainContainer: {
+    flex:1,
+    flexDirection: 'row',
+    backgroundColor:"#ECF9FF"
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  firstPart: {
+    width: '20%',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  secondPart: {
+    width: '40%',
   },
-  highlight: {
-    fontWeight: '700',
+  thirdPart: {
+    width: '30%',
   },
 });
 
---
+++ components/widgets/TotalAlarms.tsx
@@ -0,0 +1,143 @@
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {moderateScale} from 'react-native-size-matters';
+import Filter from '../images/filter.png';
+
+const TotalAlarms = () => {
+  const [totalAlarms, setTotalAlarms] = useState([]);
+
+  const today = new Date();
+  console.log('today', today);
+  const day = String(today.getDate()).padStart(2, '0');
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const year = today.getFullYear();
+  // const formattedDate = today.toISOString().split('T')[0];
+  const formattedDate = `${day}-${month}-${year}`;
+  console.log(formattedDate);
+  const apiDate = `${year}-${month}-${day}`;
+  console.log('apidate', apiDate);
+
+  const limitedData = totalAlarms?.slice(0, 10);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Define the API endpoint URL
+        const apiUrl =
+          'https://test.g-trackit.com:8090/apis/v1/vehicles/alarms?date=2023-07-19';
+        const headers = {
+          'Content-Type': 'application/json',
+          'Api-Key': 'zaeemkey1',
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImFjY2Vzc0NvZGVzIjpbIjEiLCIyIiwiMyJdLCJpZCI6MSwicm9sZSI6IjAiLCJtb2JpbGUiOiIxMDAwMiIsImVtYWlsIjoiIiwicm9sZVZhbCI6Im51bGwiLCJpc09wZXJhdG9yIjowLCJpc3MiOiJaYWVlbSIsImlhdCI6MTY4ODA5Njg4NCwiZXhwIjoxNjkwNjg4ODg0fQ.cbyybtJ1ZJLc906Jc2Fk0O1hGZvRUNUKchRz_X2aL_c',
+        };
+        const response = await fetch(apiUrl, {
+          headers: headers,
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        // console.log('alarmdata', data);
+        console.log('length', data?.length);
+        setTotalAlarms(data);
+        console.log('type', typeof data[0].createtime);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <View style={styles.mainAlarm}>
+      <View style={styles.heading}>
+        <Text style={styles.alarmHeading}>
+          Total Alarms:{' '}
+          <Text style={{color: '#db676b', fontSize: moderateScale(18)}}>
+            {totalAlarms?.length}
+          </Text>
+        </Text>
+        <Text>{formattedDate}</Text>
+      </View>
+      <View style={styles.headerRow}>
+        <Text style={styles.headerCell1}>Time</Text>
+        <Text style={styles.headerCell}>Plate Number</Text>
+        <Text style={styles.headerCell}>Alarm</Text>
+      </View>
+      {limitedData?.map((rowData, index) => (
+        <View key={index} style={styles.row}>
+          <Text style={styles.cell1}>{rowData?.createtime.split(' ')[1]}</Text>
+          <Text style={styles.cell2}>{rowData?.plate}</Text>
+          <Text style={styles.cell3}>{rowData?.statusText}</Text>
+        </View>
+      ))}
+      <View style={styles.line} />
+      {/* <Filter /> */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainAlarm: {
+    width: '100%',
+    // height: 300,
+    backgroundColor: '#fff',
+    paddingVertical: moderateScale(10),
+    paddingHorizontal: moderateScale(8),
+    fontWeight: 'bold',
+    borderRadius: moderateScale(4),
+  },
+  alarmHeading: {
+    color: '#000',
+    fontSize: moderateScale(10),
+    fontWeight: '600',
+  },
+  heading: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: moderateScale(8),
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: moderateScale(8),
+    // backgroundColor:"#C6EBFD"
+  },
+  headerRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: moderateScale(8),
+    backgroundColor: '#C6EBFD',
+    paddingVertical: moderateScale(5),
+    color: '#000',
+  },
+  headerCell: {
+    flex: 1,
+    fontWeight: '500',
+    color: '#000',
+  },
+  headerCell1: {
+    flex: 1,
+    fontWeight: '500',
+    paddingLeft: moderateScale(5),
+    color: '#000',
+  },
+  cell1: {
+    flex: 1,
+    paddingLeft: moderateScale(5),
+  },
+  cell2: {flex: 1},
+  cell3: {
+    flex: 1,
+  },
+  line: {
+    height: 1,
+    width: '100%',
+    backgroundColor: '#bbb',
+  },
+});
+
+export default TotalAlarms;
